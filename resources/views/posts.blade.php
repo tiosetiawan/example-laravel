ea7a89debc5e210ba8828591b56d988ea9a19211
tiosetiawan <tiosetiawan11@gmail.com>
2022-05-23 16:01:09 +0800

    belajar laravel redesign UI

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -8,9 +8,11 @@
 
 @if ($posts->count())
 <div class="card mb-3">
-    <img src="https://source.unsplash.com/1200x400/?{{ $posts[0]->category->name }}" class="card-img-top" alt="{{ $posts[0]->category->name }}">
+    <img src="https://source.unsplash.com/1200x400?{{ $posts[0]->category->name }}" class="card-img-top"
+        alt="{{ $posts[0]->category->name }}">
     <div class="card-body text-center">
-        <h3 class="card-title "> <a href="/posts/{{ $posts[0]->slug  }}" class="text-decoration-none text-dark"> {{ $posts[0]->title }}</a></h3>
+        <h3 class="card-title "> <a href="/posts/{{ $posts[0]->slug  }}" class="text-decoration-none text-dark">
+                {{ $posts[0]->title }}</a></h3>
         <p>
             <small class="text-muted">
                 By <a href="/authors/{{ $posts[0]->author->username }}" class="text-decoration-none">
@@ -18,7 +20,7 @@
                 in <a href="/categories/{{ $posts[0]->category->slug }}" class="text-decoration-none">
                     {{ $posts[0]->category->name }}
                 </a> {{ $posts[0]->created_at->diffForHumans() }}</p>
-            </small>
+        </small>
         <p class="card-text">{{ $posts[0]->excerpt }}</p>
         <a href="/posts/{{ $posts[0]->slug }}" class="text-decoration-none btn btn-primary btn-sm">Read more</a>
     </div>
@@ -30,35 +32,26 @@
 <div class="container">
     <div class="row">
         @foreach ($posts->skip(1) as $post)
-        <div class="col-md-4">
+        <div class="col-md-4 mb-2">
             <div class="card">
-                <img src="https://source.unsplash.com/500x400/?{{ $post->category->name }}" class="card-img-top" alt="{{ $post->category->name }}">
+                <div class="position-absolute px-3 py-2 rounded-end" style="background-color: rgba(0, 0, 0, 0.7)"><a href="/posts/{{ $post->slug }}" class="text-decoration-none text-white">{{ $post->category->name }}</a> </div>
+                <img src="https://source.unsplash.com/500x400?{{ $post->category->name }}" class="card-img-top"
+                    alt="{{ $post->category->name }}">
                 <div class="card-body">
-                  <h5 class="card-title"> <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a></h5>
-                  <small class="text-muted">
-                    By <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">
-                        {{ $post->author->name }}</a>
-                     {{ $post->created_at->diffForHumans() }}
-                </small>
-                  <p class="card-text">{{ $post->excerpt }}</p>
-                  <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
+                    <h5 class="card-title"> <a href="/posts/{{ $post->slug }}"
+                            class="text-decoration-none">{{ $post->title }}</a></h5>
+                    <small class="text-muted">
+                        By <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">
+                            {{ $post->author->name }}</a>
+                        {{ $post->created_at->diffForHumans() }}
+                    </small>
+                    <p class="card-text">{{ $post->excerpt }}</p>
+                    <a href="/posts/{{ $post->slug }}" class="btn btn-primary">Read more</a>
                 </div>
-              </div>
+            </div>
         </div>
         @endforeach
     </div>
 </div>
 
-@foreach ($posts->skip(1) as $post)
-<article class="mb-5 border-bottom pb-4">
-    <h2>
-        <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
-    </h2>
-    <p>By <a href="/authors/{{ $post->author->username }}" class="text-decoration-none"> {{ $post->author->name }}</a>
-        in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none"> {{ $post->category->name }}
-        </a></p>
-    <p>{{ $post->excerpt }}</p>
-    <a href="/posts/{{ $post->slug }}" class="text-decoration-none">Read more ..</a>
-</article>
-@endforeach
 @endsection
