5f45d5c3926315a235dca23afa13e02552665720
tiosetiawan <tiosetiawan11@gmail.com>
2022-05-24 15:59:14 +0800

    resgister dan login

--- routes/web.php
+++ routes/web.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\RegisterController;
+use App\Http\Controllers\DashboardController;
 use Illuminate\Routing\Route as RoutingRoute;
 
 
@@ -65,6 +66,11 @@ Route::get('/authors/{author:username}', function(User $author){
     ]);
 });
 
-Route::get('/login', [LoginController::class,'index']);
-Route::get('/register', [RegisterController::class,'index']);
+Route::get('/login', [LoginController::class,'index'])->middleware('guest');
+Route::post('/login', [LoginController::class,'authenticate']);
+Route::post('/logout', [LoginController::class,'logout']);
 
+Route::get('/register', [RegisterController::class,'index']);
+Route::post('/register', [RegisterController::class,'store']);
+
+Route::get('/dashboard', [DashboardController::class,'index'])->middleware('auth');
