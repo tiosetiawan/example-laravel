b8d76b353acae87a01f3d012339705d7be31844a
tiosetiawan <tiosetiawan11@gmail.com>
2022-05-24 20:09:59 +0800

    ui dashboard

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -45,5 +45,15 @@ class Post extends Model
     public function author(){
         return $this->belongsTo(User::class, 'user_id');
     }
+
+    /**
+     * Get the route key for the model.
+     *
+     * @return string
+     */
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
 }
 
---
+++ resources/views/dashboard/posts/show.blade.php
@@ -0,0 +1,23 @@
+@extends('dashboard.layouts.main')
+
+@section('container')
+<main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-8 my-3">
+                <h2>{{ $post->title  }}</h2>
+                <a href="/dashboard/posts" class="btn btn-success btn-sm"> <span data-feather="arrow-left"></span> Back To All My
+                    Posts</a>
+                <a href="" class="btn btn-warning btn-sm"><span data-feather="edit"></span>Edit</a>
+                <a href="" class="btn btn-danger btn-sm"> <span data-feather="x-circle"></span>Delete</a>
+                <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}"
+                    class="img-fluid rounded mt-2" alt="{{ $post->category->name }}">
+
+                <article class="my-3 fs-5">
+                    {!! $post->body !!}
+                </article>
+            </div>
+        </div>
+    </div>
+</main>
+@endsection
